d2ede9ba6b3c13635d5c2d47fea2148186cc703e
LLL <2798142644@qq.com>
2026-08-17 06:44:57 -0400

    11

---
+++ agent/tianji/nodes/__init__.py
@@ -0,0 +1,9 @@
+import os, sys
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+from IntentAgent import IntentAgent
+from BuyAgent import BuyAgent
+from ConsultAgent import ConsultAgent
+from KnowledgeAgent import KnowledgeAgent
+from RecommendAgent import RecommendAgent
+from UnknownAgent import UnknownAgent
